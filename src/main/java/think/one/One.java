7bcd421b72b1e5c111f6f93d37c00731b8d9b53f
roman <lotockijj-roman@rambler.ru>
2023-04-03 16:33:40 +0300

    Done third task in thinking like a programmer; deleted target folder and added it to .gitigore

--- src/main/java/think/one/One.java
+++ src/main/java/think/one/One.java
@@ -18,43 +18,75 @@ an end-of-line), write a program that produces the following shape:
  ######
   ####
    ##
+3. Here’s an especially tricky one:
+#            #
+ ##        ##
+  ###    ###
+   ########
+   ########
+  ###    ###
+ ##        ##
+#            #
 */
-
-import java.util.Set;
 
 public class One {
 
     public static void main(String[] args) {
         //printRequiredShape();
-        printSecondRequiredShape(); //lotockijj-roman@rambler.ru lotockijj-roman@rambler.ru
-        Set<Integer> set  = null;
-    }
-    //git config --global user.email "lotockijj-roman@rambler.ru"
-
-
-    private static void printSecondRequiredShape() {
+        //printSecondRequiredShape();
+        printThirdRequiredShape();
         for (int i = 0; i < 8; i++) {
-            for (int j = 0 ;j < 8; j++) {
-                if(i >= j && j > 8 - i)  {
-                    System.out.print("#");
-                } else{
-                    System.out.print(" ");
+            if((i > 0 && i <= 4) || (i >= 4 && i <=6)){
+                //System.out.print("#");
+            } else {
+                //System.out.print("0");48.59 movie !!!
+            }
+        }
+    }
+
+    private static void printThirdRequiredShape() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0; j < 14; j++) {
+                if(i <= 3   && ((j >= i && j <= i*2)                         || (j > 12 - i*2 && j < 14 - i)) ||
+                (i > 3      && ((j >= 8 - i - 1) && (j <= (8 - i - 1)*2)     || j >= i*2 - 1 && j - i <=6))){
+                    stringBuilder.append("#");
+                } else {
+                    stringBuilder.append(" ");
                 }
             }
-            System.out.println();
+            stringBuilder.append("\n");
         }
+       System.out.print(stringBuilder);
+    }
+
+    private static void printSecondRequiredShape() {
+        StringBuilder stringBuilder = new StringBuilder();
+        for (int i = 0; i < 8; i++) {
+            for (int j = 0 ;j < 8; j++) {
+                if(Math.abs(i - 3.5) + Math.abs(j - 3.5) <= 4)  {
+                    stringBuilder.append("#");
+                } else{
+                    stringBuilder.append(" ");
+                }
+            }
+            stringBuilder.append("\n");
+        }
+        System.out.println(stringBuilder);
     }
 
     private static void printRequiredShape() {
+        StringBuilder stringBuilder = new StringBuilder();
         for (int i = 0; i < 4; i++) {
             for (int j = 0 ;j < 8; j++) {
                 if(i <= j && j < 8 - i)  {
-                    System.out.print("#");
+                    stringBuilder.append("#");
                 } else{
-                    System.out.print(" ");
+                    stringBuilder.append(" ");
                 }
             }
-            System.out.println();
+            stringBuilder.append("\n");
         }
+        System.out.println(stringBuilder);
     }
 }
