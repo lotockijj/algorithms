5ce1d06b688924af9c72da70fbd8e5aa9f3723bf
roman <lotockijj-roman@rambler.ru>
2023-04-11 11:05:33 +0300

    Algorithms: Exercise 3.5.4

--- src/main/java/three/five/Exercise_3_5_1__3_5_4.java
+++ src/main/java/three/five/Exercise_3_5_1__3_5_4.java
@@ -1,10 +1,14 @@
 package three.five;
 
 /*
-Implement SET and HashSET as “wrapper class” clients of ST and HashST, respectively (provide dummy values and ignore them).
- */
+3.5.1 Implement SET and HashSET as “wrapper class” clients of ST and HashST, respectively (provide dummy values and ignore them).
 
-public class Exercise_3_5_1 {
+3.5.4 Develop classes HashSTint and HashSTdouble for maintaining sets of keys of
+primitive int and double types, respectively. (Convert generics to primitive types in
+the code of LinearProbingHashST.)
+*/
+
+public class Exercise_3_5_1__3_5_4 {
 
     public static void main(String[] args) {
         ST st = new ST();
