2631ecb3f7b32f7d7fc11903886336577840baa4
roman <lotockijj-roman@rambler.ru>
2023-04-12 19:10:46 +0300

    Algorithms: Exercise 3.5.12

--- src/main/java/three/five/Exercise_3_5_12.java
+++ src/main/java/three/five/Exercise_3_5_12.java
@@ -5,5 +5,31 @@ Modify LookupCSV to associate with each key all values that appear in a key-valu
 in the input (not just the most recent, as in the associative-array abstraction).
  */
 
+import edu.princeton.cs.algs4.In;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 public class Exercise_3_5_12 {
+    private static final String FILE_PATH = "C:\\Users\\Роман\\Downloads\\alglorithms2\\exercise_3_5_12.txt";
+
+    public static void main(String[] args) {
+        In in = new In("C:\\Users\\Роман\\Downloads\\alglorithms2\\exercise_3_5_12.txt");
+        LinearProbingHashST_3_5_8<String, String> hashST358 = new LinearProbingHashST_3_5_8<>();
+        while(in.hasNextLine()){
+            String line = in.readLine();
+            String[] tokens = line.split(",");
+            String key = tokens[0];
+            List<String> values = new ArrayList<>(Arrays.asList(tokens[1], tokens[2]));
+            hashST358.put(key, values);
+        }
+        List<String> keys = hashST358.getKeys();
+        List<String> vals = hashST358.getVals();
+        for (int i = 0, j = 1; i < keys.size(); i++, j += 2) {
+            String key = keys.get(i);
+            String values = vals.get(j - 1) + ":" + vals.get(j);
+            System.out.println(key + " -> " + values);
+        }
+    }
 }
