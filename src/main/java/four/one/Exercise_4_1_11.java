5d2d816d8beefab1be47fe17a8122a7fbcb71d1a
Roman Lotockiy <roman.lotockiy@wearebrain.com>
2023-05-04 08:29:03 +0300

    Algorithms: exercise 4.1.11

---
+++ src/main/java/four/one/Exercise_4_1_11.java
@@ -0,0 +1,12 @@
+package four.one;
+
+/*
+Draw the tree represented by edgeTo[] after the call bfs(G, 0) in Algorithm 4.2 for the graph built by
+Graph’s input stream constructor for the file tinyGex2.txt (see Exercise 4.1.2).
+ */
+
+public class Exercise_4_1_11 {
+    // 0 -> 6
+    // 0 -> 2 -> 5 -> 10
+    // 0 -> 3
+}
